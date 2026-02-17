c9c81bdcbd2a07fb2f7560f939c08a42f0cf13d3
mark jones <markstephenjones@outlook.com>
2026-02-17 15:14:50 +0000

    Initial Commit: Base SwiftUI Project

---
+++ List&Detail/List_DetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_DetailApp.swift
+//  List&Detail
+//
+//  Created by mark jones on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct List_DetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
